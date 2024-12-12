a840d14ab851a0e3fc92b5564f49db4adef23cf5
NIKHILBz <nikhil.21911203@viit.ac.in>
2024-12-12 17:26:04 +0530

    models01

---
+++ Models/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace To_Do_List_App.Models
+{
+    public class Status
+    {
+        public string StstusId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
